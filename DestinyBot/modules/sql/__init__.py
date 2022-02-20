933864be11fe1094e03d1ab928b34f10da0a26e5
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-02-20 10:51:25 +0530

    Update __init__.py

--- DestinyBot/modules/sql/__init__.py
+++ DestinyBot/modules/sql/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from DestinyBotRobot import DB_URL
+from DestinyBot import DB_URL
 
 
 def start() -> scoped_session:
