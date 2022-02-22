b6cb53dfe9797cf15b65f5ef74e693384fc780fa
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-02-22 16:38:11 +0530

    Update disasters.py

--- DestinyBot/modules/disasters.py
+++ DestinyBot/modules/disasters.py
@@ -652,8 +652,7 @@ Group admins/group owners do not need these commands.
  ╠ ✮ `/addmusicart` or `/addsudo`*:* Adds a user to Dragon
  ╠ ✮ `/addd2slayer` or `/addsupport`*:* Adds a user to Demon
  ╠ ✮ `/adddefender` or `/addtiger`*:* Adds a user to Tiger
- ╠ ✮ `/addmelcr` or `/addwolf`*:* Adds a user to Wolf
- ╚ ✮ `Add dev doesnt exist, devs should know how to add themselves`
+ ╚ ✮ `/addmelcr` or `/addwolf`*:* Adds a user to Wolf 
  ╔ *Ping:*
  ╠ ✮ `/ping`*:* gets ping time of bot to telegram server
  ╚ ✮ `/pingall`*:* gets all listed ping times
@@ -663,24 +662,29 @@ Group admins/group owners do not need these commands.
  ╠ ✮ `/broadcastusers`*:* Broadcasts too all users
  ╚ ✮ `/broadcastgroups`*:* Broadcasts too all groups
  ╔ *Groups Info:*
- ╠ ✮ `/groups`*:* List the groups with Name, ID, members count as a txt
+ ╠ ✮ `/groups`*:* List the groups with Name, ID, members
+ ╠    count as a txt
  ╠ ✮ `/leave <ID>`*:* Leave the group, ID must have hyphen
  ╠ ✮ `/stats`*:* Shows overall bot stats
- ╠ ✮ `/getchats`*:* Gets a list of group names the user has been 
-seen in. Bot owner only
- ╚ ✮ `/ginfo username/link/ID`*:* Pulls info panel for entire group
+ ╠ ✮ `/getchats`*:* Gets a list of group names the user has 
+ ╠    been seen in. Bot owner only
+ ╚ ✮ `/ginfo username/link/ID`*:* Pulls info panel for 
+       entire group
  ╔ *Access control:* 
  ╠ ✮ `/ignore`*:* Blacklists a user from 
- ╠  using the bot entirely
+ ╠     using the bot entirely
  ╠ ✮ `/notice`*:* Removes user from blacklist
  ╚ ✮ `/ignoredlist`*:* Lists ignored users
  ╔ *Windows self hosted only:*
  ╠ ✮ `/reboot`*:* Restarts the bots service
- ╚ ✮ `/gitpull`*:* Pulls the repo and then restarts the bots service
+ ╚ ✮ `/gitpull`*:* Pulls the repo and then restarts the
+       bots service
  ╔ *Chatbot:* 
- ╚ ✮ `/listaichats`*:* Lists the chats the chatmode is enabled in
+ ╚ ✮ `/listaichats`*:* Lists the chats the chatmode
+       is enabled in
  ╔ *Global Bans:*
- ╠ ✮ `/gban <id> <reason>`*:* Gbans the user, works by reply too
+ ╠ ✮ `/gban <id> <reason>`*:* Gbans the user,
+       works by reply too
  ╠ ✮ `/ungban`*:* Ungbans the user, same usage as gban
  ╚ ✮ `/gbanlist`*:* Outputs a list of gbanned users
 
