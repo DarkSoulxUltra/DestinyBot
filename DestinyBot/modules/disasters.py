13800c6bd24fe386468a2ad3b963f51dd0c0e684
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-02-22 16:21:37 +0530

    Update disasters.py

--- DestinyBot/modules/disasters.py
+++ DestinyBot/modules/disasters.py
@@ -638,6 +638,55 @@ def devlist(update: Update, context: CallbackContext):
     update.effective_message.reply_text(reply, parse_mode=ParseMode.HTML)
 
 
+__help__ = """
+*⚠️ Notice:*
+Commands listed here only work for users with special
+access are mainly used for troubleshooting, debugging purposes.
+Group admins/group owners do not need these commands. 
+ ╔ *List all special users:*
+ ╠ ✮ `/musicarts` or `/sudolist`*:* Lists all Dragon disasters
+ ╠ ✮ `/d2slayers` or `/supportlist`*:* Lists all Demon disasters
+ ╠ ✮ `/defenders` or `/tigerlist`*:* Lists all Tigers disasters
+ ╠ ✮ `/mel_creators` or `/whitelist`*:* Lists all Wolf disasters
+ ╠ ✮ `/conductors` or `/devlist`*:* Lists all Dev disastors
+ ╠ ✮ `/addmusicart` or `/addsudo`*:* Adds a user to Dragon
+ ╠ ✮ `/addd2slayer` or `/addsupport`*:* Adds a user to Demon
+ ╠ ✮ `/adddefender` or `/addtiger`*:* Adds a user to Tiger
+ ╠ ✮ `/addmelcr` or `/addwolf`*:* Adds a user to Wolf
+ ╚ ✮ `Add dev doesnt exist, devs should know how to add themselves`
+ ╔ *Ping:*
+ ╠ ✮ `/ping`*:* gets ping time of bot to telegram server
+ ╚ ✮ `/pingall`*:* gets all listed ping times
+ ╔ *Broadcast: (Bot owner only)*
+ ╠  *Note:* This supports basic markdown
+ ╠ ✮ `/broadcastall`*:* Broadcasts everywhere
+ ╠ ✮ `/broadcastusers`*:* Broadcasts too all users
+ ╚ ✮ `/broadcastgroups`*:* Broadcasts too all groups
+ ╔ *Groups Info:*
+ ╠ ✮ `/groups`*:* List the groups with Name, ID, members count as a txt
+ ╠ ✮ `/leave <ID>`*:* Leave the group, ID must have hyphen
+ ╠ ✮ `/stats`*:* Shows overall bot stats
+ ╠ ✮ `/getchats`*:* Gets a list of group names the user has been 
+seen in. Bot owner only
+ ╚ ✮ `/ginfo username/link/ID`*:* Pulls info panel for entire group
+ ╔ *Access control:* 
+ ╠ ✮ `/ignore`*:* Blacklists a user from 
+ ╠  using the bot entirely
+ ╠ ✮ `/notice`*:* Removes user from blacklist
+ ╚ ✮ `/ignoredlist`*:* Lists ignored users
+ ╔ *Windows self hosted only:*
+ ╠ ✮ `/reboot`*:* Restarts the bots service
+ ╚ ✮ `/gitpull`*:* Pulls the repo and then restarts the bots service
+ ╔ *Chatbot:* 
+ ╚ ✮ `/listaichats`*:* Lists the chats the chatmode is enabled in
+ ╔ *Global Bans:*
+ ╠ ✮ `/gban <id> <reason>`*:* Gbans the user, works by reply too
+ ╠ ✮ `/ungban`*:* Ungbans the user, same usage as gban
+ ╚ ✮ `/gbanlist`*:* Outputs a list of gbanned users
+
+Visit @unmei_support for more information.
+
+"""
 
 DEV_HANDLER = CommandHandler(("adddev", "addconductor"), adddev)
 SUDO_HANDLER = CommandHandler(("addsudo", "addmusicart"), addsudo)
@@ -675,6 +724,8 @@ dispatcher.add_handler(TIGERLIST_HANDLER)
 dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
+
+__mod_name__ = "Disasters"
 
 __handlers__ = [
     DEV_HANDLER, SUDO_HANDLER, SUPPORT_HANDLER, TIGER_HANDLER, WHITELIST_HANDLER,
