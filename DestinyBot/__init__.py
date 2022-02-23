ea7e323bda98d778e9a300817a35c5f135d2b4dc
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-02-23 23:50:51 +0530

    Update __init__.py

--- DestinyBot/__init__.py
+++ DestinyBot/__init__.py
@@ -130,7 +130,7 @@ if ENV:
     BOT_ID = int(os.environ.get("BOT_ID", None))
     ARQ_API_URL = "https://thearq.tech/"
     ARQ_API_KEY = os.environ.get("ARQ_API_KEY", None)
-    ARQ_API_KEY = "JSTHYI-JSMIOA-KSAEWA-GQLTYS-ARQ"
+    # ARQ_API_KEY = "JSTHYI-JSMIOA-KSAEWA-GQLTYS-ARQ"
     ARQ_API = ARQ_API_KEY
     ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
 
