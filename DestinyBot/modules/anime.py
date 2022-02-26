46aa1e1cdc222ca1b447f0d2523e53f6aed1ddb8
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-02-26 14:47:58 +0530

    Update anime.py

--- DestinyBot/modules/anime.py
+++ DestinyBot/modules/anime.py
@@ -239,7 +239,7 @@ def anime(update: Update, context: CallbackContext):
         msg = msg[:-2] + '`\n'
         info = json.get('siteUrl')
         trailer = json.get('trailer', None)
-        anime_id = json['id']
+        #anime_id = json['id']
         if trailer:
             trailer_id = trailer.get('id', None)
             site = trailer.get('site', None)
