9412a3c749ea388f59d9d71ec578f6a1f031a5b3
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-03-01 09:07:58 +0530

    Update misc.py

--- DestinyBot/modules/helper_funcs/misc.py
+++ DestinyBot/modules/helper_funcs/misc.py
@@ -1,5 +1,5 @@
 from typing import Dict, List
-
+from math import ceil
 from DestinyBot import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
