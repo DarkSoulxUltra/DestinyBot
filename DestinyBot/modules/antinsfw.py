52ca60b888c4d85583bd60e0e1361affa97217db
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-03-01 10:43:49 +0530

    Update antinsfw.py

--- DestinyBot/modules/antinsfw.py
+++ DestinyBot/modules/antinsfw.py
@@ -159,10 +159,9 @@ async def nsfw_enable_disable(_, message):
 
 __help__ = """
 *Anti-NSFW*
-✮ /antinsfw [on/off]*:* To enable/disable antinsfw option in your chat.
-✮ /nsfwscan *:* Reply to image/sticker/gif/video to check if it's a NSFW content or not.
-✮ /profanity [on/off] *:* To restrict the use of some profanity/slang words.
-
+  ✮ `/antinsfw [on/off]`*:* To enable/disable antinsfw option in your chat.
+  ✮ `/nsfwscan`*:* Reply to image/sticker/gif/video to check if it's a NSFW content or not.
+  ✮ `/profanity [on/off]`*:* To restrict the use of some profanity/slang words.
 Reach out to @unmei_support if you want the list of profanity words.
 """
 
