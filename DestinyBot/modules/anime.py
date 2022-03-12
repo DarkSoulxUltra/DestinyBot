2863cd0aa35f55226c0ba899e4a1795f5d6d3f46
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-03-12 12:08:00 +0530

    Update anime.py

--- DestinyBot/modules/anime.py
+++ DestinyBot/modules/anime.py
@@ -1,5 +1,6 @@
 import datetime
 import re
+from DestinyBot import telethn as tbot
 from DestinyBot.modules.helper_funcs.tools import post_to_telegraph
 from hentai import Hentai, Utils
 from natsort import natsorted
