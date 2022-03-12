37faf09cf8340af8df4e0c14b1e7b49fbdb6ebaf
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-03-12 13:01:07 +0530

    Update nsfw.py

--- DestinyBot/modules/nsfw.py
+++ DestinyBot/modules/nsfw.py
@@ -91,7 +91,7 @@ async def nhentai(event):
     message_id = event.message.id
     input_str = event.pattern_match.group(1)
     code = input_str
-    is_nsfw = sql.is_nsfw(chat_id)
+    #is_nsfw = sql.is_nsfw(chat_id)
     if "nhentai" in input_str:
         link_regex = r"(?:https?://)?(?:www\.)?nhentai\.net/g/(\d+)"
         match = re.match(link_regex, input_str)
