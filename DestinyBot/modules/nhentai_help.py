0c6b0d6e58361b2e4267b5c1724a3603db5198c4
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-03-13 10:32:15 +0530

    Update nhentai_help.py

--- DestinyBot/modules/nhentai_help.py
+++ DestinyBot/modules/nhentai_help.py
@@ -2,10 +2,10 @@ __mod_name__ = "Nhentai"
 
 __help__ = """
 *Nhentai / Doujins*
- ✮ `/nhentai <doujin code>`*:* To get a doujin for the provided code.
- ✮ `/nhentai random`*:* It provides random doujins irrespective or type and language.
+✮ /nhentai <doujin code>*:* To get a doujin for the provided code.
+✮ /nhentai random*:* It provides random doujins irrespective or type and language.
 
-Note: 1. This will only work if you are in nsfw mode either in group or in private chat. do /addnsfw to enable nsfw mode before using it.
-      2. If you need doujin codes, you can visit [Just Doujins](https://t.me/doujin_codes).
-Reach out to @unmei_support for any queries.
+Note:
+1. This will only work if you are in nsfw mode either in group or in private chat. do /addnsfw to enable nsfw mode before using it.
+2. If you need doujin codes, you can visit [Just Doujins](https://t.me/doujin_codes).
 """
