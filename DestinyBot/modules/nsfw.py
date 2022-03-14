cef9d3ac3b06cf24493d6a99165426a0d30bf63d
Bruhhhhhhh11 <99410022+Bruhhhhhhh11@users.noreply.github.com>
2022-03-14 23:58:10 +0530

    Update nsfw.py

--- DestinyBot/modules/nsfw.py
+++ DestinyBot/modules/nsfw.py
@@ -848,11 +848,6 @@ __handlers__ = [
 
 
 __help__ = """
-*Anti-NSFW:*
- ✮ /antinsfw [on/off]*:* To enable/disable antinsfw option in your chat.
- ✮ /nsfwscan*:* Reply to image/sticker/gif/video to check if it's a NSFW content or not.
- ✮ /profanity [on/off]*:* To restrict the use of some profanity/slang words.
-
 *NSFW:*
  ✮ /addnsfw : Enable NSFW mode
  ✮ /rmnsfw : Disable NSFW mode
