53161a63ead62ce8e79c8c548eed7ecec471a299
Bruhhhhhhh11 <99410022+Bruhhhhhhh11@users.noreply.github.com>
2022-03-17 09:09:32 +0530

    Update pokedex.py

--- DestinyBot/modules/pokedex.py
+++ DestinyBot/modules/pokedex.py
@@ -8,7 +8,7 @@ from DestinyBot import pbot as tomori
 @tomori.on_message(filters.command("pokedex"))
 async def PokeDex(_, message):
     if len(message.command) != 2:
-        await message.reply_text("/pokedex <Pokemon Name>")
+        await message.reply_text("/pokedex Pokemon Name")
         return
     pokemon = message.text.split(None, 1)[1]
     pokedex = f"https://some-random-api.ml/pokedex?pokemon={pokemon}"
