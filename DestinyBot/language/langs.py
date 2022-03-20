f9d20d122b2f77868f144d45479741d4d99c6a30
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-03-20 13:13:51 +0530

    Update langs.py

--- DestinyBot/language/langs.py
+++ DestinyBot/language/langs.py
@@ -23,7 +23,7 @@ class Language:
             if filename.endswith(".yaml"):
                 language_name = filename[:-5]
                 self.languages[language_name] = yaml.safe_load(
-                    open(r"./SiestaRobot/language/" + filename, encoding="utf8")
+                    open(r"./DestinyBot/language/" + filename, encoding="utf8")
                 )
 
     def get_languages(self) -> Dict:
