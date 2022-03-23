ea21aac2be06f71772399afa516882696077f9a7
Bruhhhhhhh11 <99410022+Bruhhhhhhh11@users.noreply.github.com>
2022-03-23 10:44:33 +0530

    Update memes_help.py

--- DestinyBot/modules/memes_help.py
+++ DestinyBot/modules/memes_help.py
@@ -6,6 +6,8 @@ Note: This module utilizes meme-api.heroku.com, which uses reddit as a source.
  ✮ `/meme`*:* To get random wholesome memes from subreddit r/wholesomememes.
  ✮ `/cursed`*:* To get random Cursed Comments posts from the subreddit r/cursedcomments.
  ✮ `/shitpost`*:* To get random memes from subreddit r/shitposting.
+ ✮ `/fbi`*:* Sends Random FBI Memes from Reddit.
+ ✮ `/teenmemes` or `/teenagers`*:* To get random memes from r/teenagers subreddit.
 
 *Sigma*
  ✮ `/sigma`*:* Sends some stored sigma posts. (Might add new posts time by time)
