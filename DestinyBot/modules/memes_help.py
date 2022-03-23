5da2b75f05622f7466c3f92cf30c9b42a18117f6
Bruhhhhhhh11 <99410022+Bruhhhhhhh11@users.noreply.github.com>
2022-03-23 10:31:03 +0530

    Create memes_help.py

---
+++ DestinyBot/modules/memes_help.py
@@ -0,0 +1,13 @@
+__mod_name__ = "Memes"
+
+_help_ = """
+*Random Memes*
+Note: This module utilizes meme-api.heroku.com, which uses reddit as a source.
+ ✮ `/meme`*:* To get random wholesome memes from subreddit r/wholesomememes.
+ ✮ `/cursed`*:* To get random Cursed Comments posts from the subreddit r/cursedcomments.
+ ✮ `/shitpost`*:* To get random memes from subreddit r/shitposting.
+
+*Sigma*
+ ✮ `/sigma`*:* Sends some stored sigma posts. (Might add new posts time by time)
+
+"""
