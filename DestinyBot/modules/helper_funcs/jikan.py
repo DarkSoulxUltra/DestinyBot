3a1a9ee9f6fb8443c1e0b1a27259d76e4f3412cf
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-04-08 12:09:29 +0530

    Create jikan.py

---
+++ DestinyBot/modules/helper_funcs/jikan.py
@@ -0,0 +1,42 @@
+import json
+import re
+import textwrap
+import time
+from io import BytesIO, StringIO
+import bs4
+import jikanpy
+import requests
+from aiohttp import ClientSession
+from jikanpy import Jikan
+
+jikan = Jikan()
+
+anilisturl = "https://graphql.anilist.co"
+animnefillerurl = "https://www.animefillerlist.com/shows/"
+# Anime Helper
+
+weekdays = {
+    "monday": 0,
+    "tuesday": 1,
+    "wednesday": 2,
+    "thursday": 3,
+    "friday": 4,
+    "saturday": 5,
+    "sunday": 6,
+}
+
+def get_weekday(dayid):
+    for key, value in weekdays.items():
+        if value == dayid:
+            return key
+
+async def get_anime_schedule(weekid):
+    "get anime schedule"
+    dayname = get_weekday(weekid)
+    result = f"✙ **Scheduled animes for {dayname.title()} are : **\n\n"
+    async with jikanpy.AioJikan() as animesession:
+        scheduled_list = (await animesession.schedule(day=dayname)).get(dayname)
+        for a_name in scheduled_list:
+            result += f"• [{a_name['title']}]({a_name['url']})\n"
+    return result, dayname
+
