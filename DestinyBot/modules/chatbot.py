87b01bf266d083f8996258a794a9fee320d8209a
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-04-24 13:32:25 +0530

    Update chatbot.py

--- DestinyBot/modules/chatbot.py
+++ DestinyBot/modules/chatbot.py
@@ -116,7 +116,7 @@ def chatbot(update: Update, context: CallbackContext):
             return
         Message = message.text
         bot.send_chat_action(chat_id, action="typing")
-        kukiurl = requests.get('https://kukiapi.xyz/api/apikey=KUKItg111XlOZ/yuzuki/moezill/message='+Message)
+        kukiurl = requests.get('https://kukiapi.xyz/api/apikey=866830519-KUKIqA8Fmb6Lz6/yuzuki/moezill/message='+Message)
         Kuki = json.loads(kukiurl.text)
         kuki = Kuki['reply']
         sleep(0.3)
